8f1bffa602363118498bfa5f89565a6f0eae3994
ndlovunigel <ndl20004@byui.edu>
2025-05-18 03:21:26 +0200

    new project practice

---
+++ fresh-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  standalone: false,
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://marketplace.canva.com/EAEzSj77_B4/2/0/1067w/canva-beige-cute-recipe-card-xDHAiGpfgKU.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
